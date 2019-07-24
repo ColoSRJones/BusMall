e4428044043112902caa413651853a3266b35254
ColoSRJones <colosrjones@gmail.com>
2019-07-24 17:28:06 -0500

    Added chartjs branch

--- js/app.js
+++ js/app.js
@@ -61,8 +61,8 @@ function render() {
 		var img = document.createElement('img');
 		img.setAttribute('src', randomProducts[i].imageUrl);
 		img.setAttribute('data-name', randomProducts[i].name);
-		img.setAttribute('width', '250px');
-		img.setAttribute('height', '250px');
+		img.setAttribute('width', '500px');
+		img.setAttribute('height', '500px');
 		img.setAttribute('hspace', '20px');
 		img.addEventListener('click', handleVote);
 		productSection.appendChild(img);
@@ -117,30 +117,30 @@ function renderChart() {
 		datasets: [{
 			label: 'times clicked',
 			backgroundColor: [
-				'rgba(255, 99, 132, 0.2)',
-				'rgba(54, 162, 235, 0.2)',
-				'rgba(255, 206, 86, 0.2)',
-				'rgba(75, 192, 192, 0.2)',
-				'rgba(153, 102, 255, 0.2)',
-				'rgba(255, 159, 64, 0.2)',
-				'rgba(255, 99, 132, 0.2)',
-				'rgba(54, 162, 235, 0.2)',
-				'rgba(255, 206, 86, 0.2)',
-				'rgba(75, 192, 192, 0.2)',
-				'rgba(153, 102, 255, 0.2)',
-				'rgba(255, 159, 64, 0.2)',
-				'rgba(255, 99, 132, 0.2)',
-				'rgba(54, 162, 235, 0.2)',
-				'rgba(255, 206, 86, 0.2)',
-				'rgba(75, 192, 192, 0.2)',
-				'rgba(153, 102, 255, 0.2)',
-				'rgba(255, 159, 64, 0.2)',
-				'rgba(255, 99, 132, 0.2)',
-				'rgba(54, 162, 235, 0.2)',
-				'rgba(255, 206, 86, 0.2)',
-				'rgba(75, 192, 192, 0.2)',
-				'rgba(153, 102, 255, 0.2)',
-				'rgba(255, 159, 64, 0.2)'
+				'rgba(255, 99, 132, 2.6)',
+				'rgba(54, 162, 235, 2.7)',
+				'rgba(255, 206, 86, 2.8)',
+				'rgba(75, 192, 192, 2.9)',
+				'rgba(153, 102, 255, 3)',
+				'rgba(255, 159, 64, 3.1)',
+				'rgba(255, 99, 132, 3.2)',
+				'rgba(54, 162, 235, 3.3)',
+				'rgba(255, 206, 86, 3.4)',
+				'rgba(75, 192, 192, 1)',
+				'rgba(153, 102, 255, 1.1)',
+				'rgba(255, 159, 64, 1.2)',
+				'rgba(255, 99, 132, 1.3)',
+				'rgba(54, 162, 235, 1.4)',
+				'rgba(255, 206, 86, 1.5)',
+				'rgba(75, 192, 192, 1.6)',
+				'rgba(153, 102, 255, 1.7)',
+				'rgba(255, 159, 64, 1.8)',
+				'rgba(255, 99, 132, 2)',
+				'rgba(54, 162, 235, 2.1)',
+				'rgba(255, 206, 86, 2.2)',
+				'rgba(75, 192, 192, 2.3)',
+				'rgba(153, 102, 255, 2.4)',
+				'rgba(255, 159, 64, 2.5)'
 			],
 			borderColor: [
 				'rgba(255, 99, 132, 1)',
